7d14fd3678d59b76e5e2effdc7bcac1940fd8b75
PRIME-07 <anujm0707@gmail.com>
2024-07-07 11:13:41 +0530

    made chatbot with openAI LLM

---
+++ app_open_ai.py
@@ -0,0 +1,41 @@
+# Using OpenAI LLM
+from langchain_openai import ChatOpenAI  # won't work cuz it's paid, use opensource
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+
+import streamlit as st 
+import os
+from dotenv import load_dotenv
+
+# Load files from .evn file
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# LangSmith
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+
+
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the queries"),
+        ("user", "Question:{question}") 
+    ]
+) 
+
+
+# Streamlit Framework
+st.title("LangChain Demo")
+input_text = st.text_input("Ask Me Anything ")
+
+
+# OpenAI LLM
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser 
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
+
